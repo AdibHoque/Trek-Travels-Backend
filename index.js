a80d0c63c2dd9a17b33af3e485c357977ded9cef
AdibHoque <adibhoque04@gmail.com>
2024-04-27 14:01:23 +0600

    Connecting to MongoDB

--- index.js
+++ index.js
@@ -7,6 +7,33 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.URI
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("API server for Trek Travels.")
 })
@@ -14,3 +41,4 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log("Listening to Port: ", port)
 })
+
