67974f51a11f0a2ddf4fcf95f85995d2ec4c3f24
AdibHoque <adibhoque04@gmail.com>
2024-04-27 14:24:28 +0600

    Added GET method to send data for '/touristspots'

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.URI
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.knvnnno.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -23,13 +24,21 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+
+    const spotCollection = client.db('TrekTravels').collection('touristSpot');
+
+    app.get('/touristspots', async (req, res) => {
+      const cursor = spotCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
